260b3bad71e590c00331073896887ae19eaccc35
Andy <60999770+Hi-Im-Andy@users.noreply.github.com>
2022-11-14 18:24:49 -0800

    Modified shapes.cpp

--- Project support code/BasicOpenGLProject/shapes.cpp
+++ Project support code/BasicOpenGLProject/shapes.cpp
@@ -1,6 +1,6 @@
 #include "shapes.h"
 
-
+///////////////////////////////// Rectangle /////////////////////////////////
 Rectangle::Rectangle(){
     length = 1;
     width = 1;
@@ -21,10 +21,84 @@ void Rectangle::push_vert(vert corner){
 
 vector<float> Rectangle::update(){
     vertices.clear();
-    vert point;
+    vert blb, brb, flb, frb, blt, brt, flt, frt;
+    // Back left bottom
+    blb.x = 0;
+    blb.y = 0;
+    blb.z = 0;
+    // back right bottom
+    brb.x = 0;
+    brb.y = width;
+    brb.z = 0;
+    // front left bottom
+    flb.x = width;
+    flb.y = 0;
+    flb.z = 0;
+    // front rigt top
+    frb.x = width;
+    frb.y = length;
+    frb.z = 0;
 
-    // Need to calculate the 8 corners in a rectangle
-    // Centered?
+    // Top corners
+    blt = blb;
+    blt.z = height;
+    brt = brb;
+    brt.z = height;
+    flt = flb;
+    flt.z = height;
+    frt = frb;
+    frt.z = height;
+
+    // Pushing them into the vector
+    // Notation front/back , left/right, top/bottom
+    // ex. blb is back left bottom
+    // Bottom face
+    push_vert(blb);
+    push_vert(brb);
+    push_vert(flb);
+    push_vert(flb);
+    push_vert(frb);
+    push_vert(brb);
+
+    // Top face
+    push_vert(blt);
+    push_vert(brt);
+    push_vert(flt);
+    push_vert(flt);
+    push_vert(frt);
+    push_vert(brt);
+
+    // Front face
+    push_vert(flt);
+    push_vert(frt);
+    push_vert(frb);
+    push_vert(flt);
+    push_vert(flb);
+    push_vert(frb);
+
+    // Back face
+    push_vert(blt);
+    push_vert(brt);
+    push_vert(brb);
+    push_vert(blt);
+    push_vert(blb);
+    push_vert(brb);
+
+    // Left face
+    push_vert(flt);
+    push_vert(blt);
+    push_vert(blb);
+    push_vert(flt);
+    push_vert(flb);
+    push_vert(blb);
+
+    // Right face
+    push_vert(frt);
+    push_vert(brt);
+    push_vert(brb);
+    push_vert(frt);
+    push_vert(frb);
+    push_vert(brb);
 };
 
 vector<float> Rectangle::fill(float r, float g, float b){ // Populates the color vector with desired color
